a6659c76295fc5b525463f1728b5379e7c4d8d50
noor-ali-346 <73874131+noor-ali-346@users.noreply.github.com>
2020-12-23 01:47:53 +1300

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop Asuna_Robot
-net start Asuna_Robot
+net stop AsunaRobot
+net start AsunaRobot
