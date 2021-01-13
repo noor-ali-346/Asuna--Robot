79540ca18a01e7a7a06b4dc0f7c8a94ec0a1bd66
Eagle owner <73874131+noor-ali-346@users.noreply.github.com>
2021-01-14 00:15:59 +0500

    Update _init_.py

--- AsunaRobot/_init_.py
+++ AsunaRobot/_init_.py
@@ -184,7 +184,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from SaitamaRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
+from AsunaRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
                                                         CustomMessageHandler,
                                                         CustomRegexHandler)
 
